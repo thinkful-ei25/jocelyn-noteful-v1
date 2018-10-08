d51482fc620b7eb9b9e2ec44d22279a9d922c113
jsantiag <jsantiag@wellesley.edu>
2018-10-08 15:38:28 -0500

    app.get

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 'use strict';
 
 // Load array of notes
+const express = require('express');
+
 const data = require('./db/notes');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(req.params);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const query = req.params;
+  let list = data; 
+  const id = req.params.id;
+  if (query.id) {
+    list = list.find(note => note.id === (id));
+  }
+  res.json(list);
+});
+
+app.listen(8080, function(){
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+
